bfecb2af0f6853b2185f537aa29ca5689c34e980
MuratSahin <sahin.mur4t@gmail.com>
2022-06-20 16:35:18 +0300

    Test added

--- contracts/src/test/java/net/corda/transafe/contracts/ContractTests.java
+++ contracts/src/test/java/net/corda/transafe/contracts/ContractTests.java
@@ -5,34 +5,41 @@ import net.corda.core.identity.AnonymousParty;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.testing.core.TestIdentity;
 import net.corda.testing.node.MockServices;
+import net.corda.transafe.states.TransferState;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import static net.corda.testing.node.NodeTestUtils.ledger;
 
 public class ContractTests {
     private final MockServices ledgerServices = new MockServices();
-    TestIdentity Operator = new TestIdentity(new CordaX500Name("Alice",  "TestLand",  "US"));
-    TestIdentity Operator2 = new TestIdentity(new CordaX500Name("Bob",  "TestLand",  "US"));
+    TestIdentity Operator1 = new TestIdentity(new CordaX500Name("IsBankasi",  "Istanbul",  "TR"));
+    TestIdentity Operator2 = new TestIdentity(new CordaX500Name("TurkTelekom",  "Ankara",  "TR"));
 
-    /*@Test
-    public void GameCanOnlyCreatedWhenTwoDifferentPlayerPresented() {
-        UniqueIdentifier playerX = new UniqueIdentifier();
-        BoardState tokenPass = new BoardState(new UniqueIdentifier(),new UniqueIdentifier(),
-                new AnonymousParty(Operator.getPublicKey()),new AnonymousParty(Operator2.getPublicKey()));
-        BoardState tokenfail = new BoardState(playerX,playerX,
-                new AnonymousParty(Operator.getPublicKey()),new AnonymousParty(Operator2.getPublicKey()));
+    @Test
+    public void TwoContractCannotHaveSameEmail() {
+        UniqueIdentifier Identity1 = new UniqueIdentifier();
+        UniqueIdentifier Identity2 = new UniqueIdentifier();
+        TransferState transferState = new TransferState("DOSYA","title" ,"senderHost",new AnonymousParty(Operator1.getPublicKey()), Identity1.toString(),
+                new AnonymousParty(Operator2.getPublicKey()),Identity2.toString(),LocalDateTime.now(),LocalDateTime.now().plusDays(2),new UniqueIdentifier(),true);
+
         ledger(ledgerServices, l -> {
             l.transaction(tx -> {
-                tx.output(BoardContract.ID, tokenfail);
-                tx.command(Operator.getPublicKey(), new BoardContract.Commands.StartGame()); // Wrong type.
-                return tx.fails();
+                tx.input(TransferContract.ID, transferState);
+                tx.output(TransferContract.ID, transferState);
+                tx.command(Operator1.getPublicKey(), new TransferContract.Commands.SendFile());
+                return tx.fails(); //fails because of having inputs
             });
             l.transaction(tx -> {
-                tx.output(BoardContract.ID, tokenPass);
-                tx.command(Operator.getPublicKey(), new BoardContract.Commands.StartGame()); // Wrong type.
+                tx.output(TransferContract.ID, transferState);
+                tx.command(Operator1.getPublicKey(), new TransferContract.Commands.SendFile());
                 return tx.verifies();
             });
             return null;
         });
-    }*/
+
+    }
 }
